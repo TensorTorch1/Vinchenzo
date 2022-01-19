8d4867d6ede70fecec93c046fd3bdcbeb713c1d0
Lolzkarry <46729989+Lolzkarry@users.noreply.github.com>
2022-01-19 16:33:54 -0600

    commiititiitt

--- src/main/java/frc/robot/subsystems/TankDrive/TankDrive.java
+++ src/main/java/frc/robot/subsystems/TankDrive/TankDrive.java
@@ -2,15 +2,16 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
+package frc.robot.subsystems.TankDrive;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.components.OutputSetterComponent;
-import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
 
-public class TankDrive extends SubsystemBase {
+
+public class TankDrive extends Subsystem {
 	private OutputSetterComponent left;
 	private OutputSetterComponent right;
 	private DifferentialDriveKinematics asdf;
@@ -25,7 +26,7 @@ public class TankDrive extends SubsystemBase {
 	public void move(double x, double w) {
 		ChassisSpeeds speeds = new ChassisSpeeds(x, 0, w);
 		DifferentialDriveWheelSpeeds speedsTwo = asdf.toWheelSpeeds(speeds);
-		speedsTwo.desaturate(maxWheelSpeed);
+		speedsTwo.normalize(maxWheelSpeed);
 		left.setOutput(speedsTwo.leftMetersPerSecond);
 		right.setOutput(speedsTwo.rightMetersPerSecond);
 	}
@@ -33,5 +34,10 @@ public class TankDrive extends SubsystemBase {
 	@Override
 	public void periodic() {
     	// This method will be called once per scheduler run
+	}
+
+	@Override
+	public void initDefaultCommand() {
+		//idk
 	}
 }
