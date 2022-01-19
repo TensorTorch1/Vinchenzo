61412c43e49b3998a3d318ca9d6d8e715c364789
Vincent Chen <productivitymaster30@gmail.com>
2022-01-19 15:56:06 -0600

    Suck my lollipop

--- src/main/java/frc/robot/subsystems/TankDrive/TankDrive.java
+++ src/main/java/frc/robot/subsystems/TankDrive/TankDrive.java
@@ -4,7 +4,9 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.wpilibj.command.SubsystemBase;
+
 import frc.robot.components.OutputSetterComponent;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
@@ -13,18 +15,18 @@ import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 public class TankDrive extends SubsystemBase {
 	private OutputSetterComponent left;
 	private OutputSetterComponent right;
-	private DifferentialDriveKinematics asdf;
+	private DifferentialDriveKinematics driveKinematic;
 	private double maxWheelSpeed = 10d;
 	/** Creates a new TankDrive. */
     public TankDrive(OutputSetterComponent left, OutputSetterComponent right) {
     	this.left = left;
     	this.right = right;
-    	this.asdf = new DifferentialDriveKinematics(0.8);
+    	this.driveKinematic = new DifferentialDriveKinematics(0.8);
     }
 
 	public void move(double x, double w) {
 		ChassisSpeeds speeds = new ChassisSpeeds(x, 0, w);
-		DifferentialDriveWheelSpeeds speedsTwo = asdf.toWheelSpeeds(speeds);
+		DifferentialDriveWheelSpeeds speedsTwo = driveKinematic.toWheelSpeeds(speeds);
 		speedsTwo.desaturate(maxWheelSpeed);
 		left.setOutput(speedsTwo.leftMetersPerSecond);
 		right.setOutput(speedsTwo.rightMetersPerSecond);
